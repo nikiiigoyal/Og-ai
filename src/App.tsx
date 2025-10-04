839de17e44fb54ee60e32522fe32327f1118a02d
nikiiigoyal <nikiiigoyal@gmail.com>
2025-10-04 13:26:55 +0530

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import HomePage from './pages/HomePage'
+
+function App() {
+  
+  return (
+    <>
+     <h1 className="text-4xl">Hello</h1>
+     <HomePage />
+     </>
+  )
+}
+
+export default App
---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,19 @@
+export default function HomePage() {
+  return (
+    <>
+      <div className="w-full h-screen overflow-hidden">
+        <video
+          autoPlay
+          loop
+          muted
+          playsInline
+          className="fixed top-0 left-0 block w-full h-full object-cover"
+        >
+          <source src="/og background video.mp4" type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
+       
+      </div>
+    </>
+  );
+}
