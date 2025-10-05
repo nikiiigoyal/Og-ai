9b7cbf7219d8fd885f64fd59568298dfe7210c3d
nikiiigoyal <nikiiigoyal@gmail.com>
2025-10-05 13:55:23 +0530

    Modified Header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -55,11 +55,11 @@ export default function Header () {
                      </div>
                      </div>
             <a>
-                <button className="bg-[#fff] text-black p-3 rounded-xl">Docs</button>
+                <button className="bg-[#fff] text-black p-3 rounded-xl hover:bg-[#cb8aff] hover:opacity-40 transform transition-bg duration-300">Docs</button>
             </a>
                 </div>
   </div>
-          
+          {/* bell icon */}
             <div className="ml-4 text-white">
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-bell-icon lucide-bell"><path d="M10.268 21a2 2 0 0 0 3.464 0"/><path d="M3.262 15.326A1 1 0 0 0 4 17h16a1 1 0 0 0 .74-1.673C19.41 13.956 18 12.499 18 8A6 6 0 0 0 6 8c0 4.499-1.411 5.956-2.738 7.326"/></svg>
             </div>
