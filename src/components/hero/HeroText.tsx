fd0f20ce0839d96c3ddf5a23f6d3e56a5584a644
nikiiigoyal <nikiiigoyal@gmail.com>
2025-10-06 14:19:07 +0530

    Add hero text

---
+++ src/components/hero/HeroText.tsx
@@ -0,0 +1,21 @@
+export default function HeroText() {
+    return(
+        <div className="flex w-full">
+            <div className="bg-[#b75fff26] rounded-[1000px] inline-flex flex-start items-center "> 
+                <div className="aspect-square w-0.3125 bg-[#b75fff] rounded-full"></div>
+                <div className="text-[#b75fff] font-semibold text-sm">DECENTRALIZED AI OPERATING SYSTEM</div>
+            </div>
+            <div>
+
+            </div>
+            <div>
+                <button></button>
+                <button></button>
+            </div>
+            
+        </div>
+
+    
+
+    )
+}
