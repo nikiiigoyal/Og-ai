51ddbdddc7697e3e696b7d15afe1641976b3696a
nikiiigoyal <nikiiigoyal@gmail.com>
2025-10-07 13:58:45 +0530

    Update heropahge

--- src/pages/HeroPage.tsx
+++ src/pages/HeroPage.tsx
@@ -1,10 +1,10 @@
+// HeroPage.jsx
 import HeroText from "../components/hero/HeroText";
+
 export default function HeroPage() {
   return (
-    <>
-    <div className="bg-white w-full">
+    <div className="bg-[#Fff] w-full min-h-screen py-17">
       <HeroText />
-      </div>
-    </>
+    </div>
   );
 }
