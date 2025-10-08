6911fdda0981b73a229019a130c12f0b347ce1f6
nikiiigoyal <nikiiigoyal@gmail.com>
2025-10-08 17:06:59 +0530

    Update heropage

--- src/pages/HeroPage.tsx
+++ src/pages/HeroPage.tsx
@@ -1,10 +1,12 @@
 // HeroPage.jsx
 import HeroText from "../components/hero/HeroText";
+import Stats from "../components/hero/Stats";
 
 export default function HeroPage() {
   return (
-    <div className="bg-[#Fff] w-full min-h-screen py-17">
+    <div className="bg-[#Fff] w-full min-h-screen py-18">
       <HeroText />
+      <Stats />
     </div>
   );
 }
