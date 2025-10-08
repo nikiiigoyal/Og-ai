1e4863cc9021645f723eca6a1338c3989c6758a7
nikiiigoyal <nikiiigoyal@gmail.com>
2025-10-08 14:01:30 +0530

    Update Herotext

--- src/components/hero/HeroText.tsx
+++ src/components/hero/HeroText.tsx
@@ -1,7 +1,7 @@
 // HeroText.jsx
 export default function HeroText() {
     return(
-        <div className="flex flex-col gap-6 max-w-4xl mx-auto p-8">
+        <div className="flex flex-col gap-6 mx-auto p-8">
             <div className="bg-[#b75fff26] opacity-100 rounded-full inline-flex items-center gap-2 px-4 py-2 self-start"> 
                 <div className="w-2 h-2 bg-[#b75fff] rounded-full"></div>
                 <div className="text-[#b75fff] font-semibold text-[10px]">DECENTRALIZED AI OPERATING SYSTEM</div>
@@ -11,7 +11,7 @@ export default function HeroText() {
             </div>
             <div className="lg:flex flex-start">
                 <button className="px-6 py-3 mb-3 mr-5 bg-black text-white rounded-lg font-semibold">Explore ØG</button>
-                <button className="px-6 py-3 bg-[#0000000d] border border-[#0003] text-black rounded-lg transition-all duration-300 font-semibold">Support ØG on X</button>
+                <button className="px-6 py-3 mb-3 mr-5 bg-[#0000000d] border border-[#0003] text-black rounded-lg transition-all duration-300 font-semibold">Support ØG on X</button>
             </div>
         </div>
     )
